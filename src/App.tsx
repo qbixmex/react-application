5640817c166bd26a8b2f5ebfbd408c39341689f4
Daniel González Briseño <qbixmex@gmail.com>
2022-10-26 18:55:35 -0500

    02-react-router-dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,7 @@
-import taco from './logo.svg'
+import Navigation from "./routes/Navigation";
 
 export default function App() {
   return (
-    <div className="text-center">
-      <h1 className="blue">React Advanced</h1>
-      <hr />
-      <img src={ taco } alt="React JS" width={ 300 } />
-    </div>
+    <Navigation />
   );
 };
---
+++ src/routes/Navigation.tsx
@@ -0,0 +1,34 @@
+import { BrowserRouter, Routes, Route, NavLink, Navigate } from "react-router-dom";
+import logo from '../logo.svg';
+
+const Navigation = () => {
+  return (
+    <BrowserRouter>
+      <div className="main-layout">
+        <nav>
+          <img src={ logo } alt="React JS" />
+          <ul>
+            <li>
+              <NavLink className={ ({ isActive }) => isActive ? 'nav-active' : '' } to='/home'>Home</NavLink>
+            </li>
+            <li>
+              <NavLink className={ ({ isActive }) => isActive ? 'nav-active' : '' } to='/about'>About</NavLink>
+            </li>
+            <li>
+              <NavLink className={ ({ isActive }) => isActive ? 'nav-active' : '' } to='/users'>Users</NavLink>
+            </li>
+          </ul>
+        </nav>
+        <Routes>
+          <Route path="/about" element={<h1>About</h1>} />
+          <Route path="/users" element={<h1>Users</h1>} />
+          <Route path="/home" element={<h1>Home</h1>} />
+
+          <Route path="/*" element={<Navigate to='/home' replace />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
+  );
+};
+
+export default Navigation;
