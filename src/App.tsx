eff2f8491ac42b846c0f9c1f57a3ed047bc7263d
Daniel González Briseño <qbixmex@gmail.com>
2022-10-26 18:28:40 -0500

    01-initial-setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,11 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import taco from './logo.svg'
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="text-center">
+      <h1 className="blue">React Advanced</h1>
+      <hr />
+      <img src={ taco } alt="React JS" width={ 300 } />
     </div>
   );
-}
-
-export default App;
+};
